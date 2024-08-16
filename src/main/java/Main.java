80543c1533e012ce5fb4249b17346edb6257ec24
Flooooooooooorian <florian.weber@neuefische.de>
2024-08-16 14:46:22 +0200

    Add sout git

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hallo cgn-java-24-4");
+        System.out.println("Git");
     }
 }
